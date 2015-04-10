a7b49f0cf30573be2ee12ddc41dcad54551c3c05
Jessica <jessica@strangepunch.com>
2015-04-09 22:35:11 -0400

    more views edits

--- app/effects/effectsViewCtrl.js
+++ app/effects/effectsViewCtrl.js
@@ -2,10 +2,11 @@
 	angular
 			.module("productManagement")
 			.controller("EffectsViewCtrl",
-						["productResource",
+						["$scope",
+						 "productResource",
 							EffectsViewCtrl]);
 	
-	function EffectsViewCtrl(productResource){
+	function EffectsViewCtrl($scope, productResource){
 		var vm = this;
 
 		productResource.query(function(data){
@@ -16,7 +17,19 @@
 
 		vm.toggleImage = function(){
 			vm.showImage = !vm.showImage;
-		}
+		};
+
+		$scope.selectEffect = function(name){
+			var tempArray=[];
+			for(var i=0; i<vm.products.length; i++){
+				if(name === vm.products[i].productName){
+					for(var x=0; x<vm.products[i].Property.length; x++){
+					  tempArray[x] = vm.products[i].Property[x];
+					}
+				}
+			}
+			vm.effectProperty = tempArray;
+		};
 	}
 
 }());
