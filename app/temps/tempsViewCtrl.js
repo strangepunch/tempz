4eeb2d1f2c90e3c7fd1a17771f6a9b7172b51d17
Jessica <jessica@strangepunch.com>
2015-04-21 15:21:06 -0400

    temp view improvements

--- app/temps/tempsViewCtrl.js
+++ app/temps/tempsViewCtrl.js
@@ -10,7 +10,13 @@
 	
 	function TempsViewCtrl($scope, tempResource, productResource, strainResource){
 		var vm = this;
-
+		$scope.orderName = '-value';
+		$scope.filterName = 'Trainwreck';
+		$scope.filterStrainName = function(name){
+			console.log(name);
+			$scope.filterName = name;
+				
+		}
 		//init stuff for temp display and bar and logic settings
 		vm.currentTemp = 'F';
 		vm.tempDisplay = 126;
@@ -18,11 +24,14 @@
 		vm.styleF={"color":"Red","font-size": "1.2em"};
 		vm.styleC={"color":"white","font-size": "0.8em"};
 		vm.effectProperty = [];
-
+		//set the temperature bar
 		vm.currentPosition = 6;
 		vm.leftT={"color":"gray"};
 		vm.rightT={"color":"black"};
 		vm.selectedTemps = [];
+		//effects array
+		vm.EffectsProductName = [];
+
 		//find and display selected temps
 		tempResource.query(function(data){
 			vm.Temps = data;
@@ -79,19 +88,47 @@
 		productResource.query(function(data){
 			vm.products = data;
 			var tempArray=[];
+			var numX = 0;
 
 			for(var i=0; i<vm.products.length; i++){
 				if(vm.tempDisplay === vm.products[i].highTemp){
 					for(var x=0; x<vm.products[i].Property.length; x++){
 					  tempArray[x] = vm.products[i].Property[x];
 					}
+					vm.EffectsProductName[numX] = vm.products[i].productName;
 				}
 			}
 			vm.effectProperty = tempArray;
+
+			strainResource.query(function(data){
+				vm.Strains = data;
+				vm.Container = [];
+				var num = 0;
+				var test = 0;
+				console.log(vm.EffectsProductName);
+				for(var x=0; x<vm.EffectsProductName.length;x++){
+					for(var i=0; i<data.length;i++){
+						for(var j=0; j<data[i].components.length;j++){
+							if(data[i].components[j].name === vm.EffectsProductName[x] && data[i].components[j].value > 0 ){
+								vm.Container[num] = {strainId: vm.Strains[i].strainId,
+										           	 strainName: vm.Strains[i].strainName,
+										             conditions: vm.Strains[i].conditions,
+										             comp:vm.Strains[i].components[j]};
+								num++;
+							}
+						}				
+					}
+					console.log("test", test++);
+					console.log("num", num);
+					
+				}
+				console.log(vm.Container);
+			});
 		});
 
 		//change between F and C
 		$scope.selectTemp = function(name){
+			vm.currentTemp = name;
 			if(name === 'C'){
 				vm.styleF={"color":"white","font-size": "0.8em"};
 				vm.styleC={"color":"Red","font-size": "1.2em"};
@@ -145,6 +182,8 @@
 			vm.countC=0;
 			var tempArray=[];
 			var num = 0;
+			var numX = 0;
+			console.log(value);
 			if (vm.currentTemp === 'F'){
 
 				for(var i=0; i<vm.products.length; i++){
@@ -153,6 +192,8 @@
 							tempArray[num] = vm.products[i].Property[x];
 							num++;
 						}
+						vm.EffectsProductName[numX] = vm.products[i].productName;
+						numX++;
 					}
 				}
 				vm.effectProperty = tempArray;
@@ -166,22 +207,38 @@
 						  tempArray[num] = vm.products[i].Property[x];
 						  num++;
 						}
+						vm.EffectsProductName[numX] = vm.products[i].productName;
+						numX++;
 					}
 				}
 				vm.effectProperty = tempArray;
 
 			}
+			
+			strainResource.query(function(data){
+				vm.Strains = data;
+				vm.Container = [];
+				var num = 0;
+				var test = 0;
+				console.log(vm.EffectsProductName);
+				for(var x=0; x<vm.EffectsProductName.length;x++){
+					for(var i=0; i<data.length;i++){
+						for(var j=0; j<data[i].components.length;j++){
+							if(data[i].components[j].name === vm.EffectsProductName[x] && data[i].components[j].value > 0 ){
+								vm.Container[num] = {strainId: vm.Strains[i].strainId,
+										           	 strainName: vm.Strains[i].strainName,
+										             conditions: vm.Strains[i].conditions,
+										             comp:vm.Strains[i].components[j]};
+								num++;
+							}
+						}				
+					}
+					console.log("test", test++);
+					console.log("num", num);
+				}
+				console.log(vm.Container);
+			});
 		}
-
-		strainResource.query(function(data){
-			vm.Strains = data;
-			console.log(vm.Strains[0].components);
-			var name = 'CBD';
-			if(vm.Strains[0].components.CBD > 0)
-			{
-				console.log('yes');
-			}
-		});
 		
 	}
 
