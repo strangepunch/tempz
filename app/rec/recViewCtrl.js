ec14a86f526911e65ad1dabb5e2db2de513d6272
Jessica <jessica@strangepunch.com>
2015-08-11 15:36:31 -0400

    update rec view

--- app/rec/recViewCtrl.js
+++ app/rec/recViewCtrl.js
@@ -26,6 +26,15 @@
 		//Storage space for goEasy seach
 		vm.userSelect = [{"condName":"", "strnName":""}];
 
+		//setup display of questions
+    	$scope.showQuestion1 = true;
+    	$scope.showQuestion2 = false;
+    	$scope.showQuestion3 = false;
+    	$scope.showAnswer1 = false;
+    	$scope.showAnswer2 = false;
+    	$scope.showAnswer3 = false;
+    	
+
 		//Populate modes
 		modeResource.query(function(data){
 			vm.Modes = data;
@@ -40,16 +49,34 @@
 			    	vm.modeName = "Flavor";
 			    	vm.centerImage = "1.png";
 			    	vm.discMode = "Taste";
+			    	$scope.showQuestion1 = true;
+    				$scope.showQuestion2 = false;
+    				$scope.showQuestion3 = false;
+    				$scope.showAnswer1 = false;
+    				$scope.showAnswer2 = false;
+    				$scope.showAnswer3 = false;
 			        break;
 			    case 2:
 			    	vm.modeName = "Effect";
 			    	vm.centerImage = "2.png";
 			    	vm.discMode = "Feel";
+			    	$scope.showQuestion1 = false;
+    				$scope.showQuestion2 = true;
+    				$scope.showQuestion3 = false;
+    				$scope.showAnswer1 = false;
+    				$scope.showAnswer2 = false;
+    				$scope.showAnswer3 = false;
 			        break;
 			    case 3:
 			    	vm.modeName = "Buds";
 			    	vm.centerImage = "3.png";
 			    	vm.discMode = "Strain";
+			    	$scope.showQuestion1 = false;
+    				$scope.showQuestion2 = false;
+    				$scope.showQuestion3 = true;
+    				$scope.showAnswer1 = false;
+    				$scope.showAnswer2 = false;
+    				$scope.showAnswer3 = false;
 			        break;
 			    case 4:
 			    	vm.modeName = "Postive";
@@ -73,13 +100,14 @@
 			}
 
 		};
-
-		//Populate flavor selection display
+/**---------Selecting answers in Flavor, Effect, Strain mode-------------**/
+		//Populate FLAVOR selection display
 		tasteResource.query(function(data){
 			vm.tasteList = data;
 		});
 		//select a taste
 		$scope.selectTaste = function(name){
+			vm.showQ1 = !vm.showQ1; //toggle the questions display off
 			vm.selectedTaste = name;
 			for (var i = 0; i<vm.tasteList.length; i++){
 				if(vm.tasteList[i].taste === name){
@@ -87,8 +115,93 @@
 					vm.modeName = vm.tasteList[i].taste;
 				}
 			};
-			vm.showQ1 = !vm.showQ1; 
 		};
+
+		//inititial list of STRAIN names for selecting
+	    strainNamesResource.query(function(data){
+			vm.strainNames = data;
+		});
+		//display and store the user selected strain name
+		$scope.selectStrain = function(name){
+	     	vm.showQ3 = !vm.showQ3; //toggle the questions display off
+	     	vm.selectedStrain = name;
+	    }
+	    //search and filter strains
+	    vm.searchAll = "";
+    	$scope.searchStrain = function(name){
+    		vm.searchAll = name;
+    	 	//console.log("search", name);
+    	 	switch(name){
+    	 		case 'All':
+    	 			vm.searchAll = "";
+    	 			strainNamesResource.query(function(data){
+						vm.strainNames = data;
+						//console.log("list of strains", vm.strainNames);
+					});
+					break;	
+    	 		case 'Sativa':
+    	 			vm.searchAll = "";
+    	 			strainNamesResource.query(function(data){
+    	 				var num = 0;
+    	 				vm.strainNames = [];
+    	 				for(var i=0; i<data.length; i++){
+    	 					if(data[i].strainType === 'Sativa'){
+    	 						vm.strainNames[num] = data[i];
+    	 						num++;
+    	 					}
+    	 				}
+						//console.log("list of Sativa", vm.strainNames);
+					});
+					break;	   
+				case 'Indica':
+					vm.searchAll = ""; 
+					strainNamesResource.query(function(data){
+    	 				var num = 0;
+    	 				vm.strainNames = [];
+    	 				for(var i=0; i<data.length; i++){
+    	 					if(data[i].strainType === 'Indica'){
+    	 						vm.strainNames[num] = data[i];
+    	 						num++;
+    	 					}
+    	 				}
+						//console.log("list of Indica", vm.strainNames);
+					});
+					break;	
+				case 'Hybrid':
+					vm.searchAll = "";
+					strainNamesResource.query(function(data){
+    	 				var num = 0;
+    	 				vm.strainNames = [];
+    	 				for(var i=0; i<data.length; i++){
+    	 					if(data[i].strainType === 'Hybrid'){
+    	 						vm.strainNames[num] = data[i];
+    	 						num++;
+    	 					}
+    	 				}
+						//console.log("list of Hybrid", vm.strainNames);
+					});
+					break;	
+				default:
+        			strainNamesResource.query(function(data){
+        				var num = 0;
+    	 				vm.strainNames = [];
+    	 				for(var i=0; i<data.length; i++){
+    	 					if(data[i].strainName == name){
+    	 						vm.strainNames[num] = data[i];
+    	 						num++;
+    	 					}
+    	 				}
+						//console.log("list of Seached", vm.strainNames);
+					});
+					break;
+    	 	} 
+    	};
+    	//search button --- clear Search
+	    $scope.clearSearch = function () {
+	        vm.searchAll = "";
+	    };
+/**---------Selecting answers in Flavor, Effect, Strain mode END------------**/
+
 
 		//this sets the ng-class to active
 		$scope.active = function(item){
@@ -108,20 +221,42 @@
     	};
 
     	//go back a step by hiding the solution screen and displaying question screen
-    	$scope.showAnswer = false;
-    	$scope.showQuestion = true;
-    	$scope.goBack = function(){
-    		$scope.showAnswer = false;
-    		$scope.showQuestion = true;
+    	$scope.goBack = function(mode){
+
+    		switch (mode){
+    			case 'A1':
+    				$scope.showAnswer1 = false;
+					$scope.showQuestion1 = true;
+					break;
+				case 'A2':
+					$scope.showAnswer2 = false;
+					$scope.showQuestion2 = true;
+					break;
+				case 'A3':
+					$scope.showAnswer3 = false;
+					$scope.showQuestion3 = true;
+					break;
+    		}
+   
     	}
 
     	//the GO button
-    	$scope.goEasy = function(){
-    		vm.showA1 = false;
+    	$scope.goEasy = function(mode){
 
-    		//hide the question screen and display solution screen
-    		$scope.showAnswer = true;
-			$scope.showQuestion = false;
+    		switch (mode){
+    			case 'Q1':
+    				$scope.showAnswer1 = true;
+					$scope.showQuestion1 = false;
+					break;
+				case 'Q2':
+					$scope.showAnswer2 = true;
+					$scope.showQuestion2 = false;
+					break;
+				case 'Q3':
+					$scope.showAnswer3 = true;
+					$scope.showQuestion3 = false;
+					break;
+    		}
 
     	};
 
@@ -129,16 +264,16 @@
     	vm.toggleQuestion = function(choice){
     		switch (choice){
     			case 'Q1':
-    				//console.log("Q1 A");
+    				//console.log("Q1");
 					vm.showQ1 = !vm.showQ1;
 					break;
 				case 'Q2':
 					//console.log("Q2");
 					vm.showQ2 = !vm.showQ2;
 					break;
-				case 'A1':
+				case 'Q3':
 					//console.log("Q2");
-					vm.showA1 = !vm.showA1;
+					vm.showQ3 = !vm.showQ3;
 					break;
     		}
 		}
